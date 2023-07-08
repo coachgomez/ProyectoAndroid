063f63b69202be2da69b1a3b43045c0c19208871
Elians <coachgm12@hotmail.com>
2023-07-07 23:39:23 -0500

    adelanto

--- app/src/main/java/com/example/proyectotrabajo/Evento.java
+++ app/src/main/java/com/example/proyectotrabajo/Evento.java
@@ -1,2 +1,26 @@
-package com.example.proyectotrabajo;public class evento {
+package com.example.proyectotrabajo;
+
+public class Evento {
+    private String Titulo;
+    private String Curso;
+    public Evento(String titulo, String curso) {
+        this.Titulo = titulo;
+        this.Curso = curso;
+    }
+
+    public String getTitulo() {
+        return Titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        Titulo = titulo;
+    }
+
+    public String getCurso() {
+        return Curso;
+    }
+
+    public void setCurso(String curso) {
+        Curso = curso;
+    }
 }
